c6081ea585cfd37cff36f940d505d58a42b2dd04
Kartik Tiwari <mekartik27@gmail.com>
2023-03-08 18:12:35 +0530

    updates

--- utility.hpp
+++ utility.hpp
@@ -18,11 +18,7 @@ void init() {
     RS1 = RS2 = RD = RM = RZ = RY = RA = RB = PC = IR = MuxB_select = MuxC_select = MuxINC_select = MuxY_select = MuxPC_select = MuxMA_select = RegFileAddrA = RegFileAddrB = RegFileAddrC = RegFileInp = RegFileAOut = RegFileBOut = MAR = MDR = opcode = numBytes = RF_Write = immed = PC_Temp = Mem_Write = Mem_Read = 0;
 }
 
-std::string hex(long n) {
-    
-}
-
-void ProcessMemInterface() {
+std::string ProcessMemInterface() {
     if(MuxMA_select == 0) {
         if(Mem_Read==1) {
             std::vector<int> t;
@@ -31,6 +27,49 @@ void ProcessMemInterface() {
             }
             reverse(t.begin(), t.end());
             std::string ans = "0x";
+            for(auto i:t) {
+                std::string curr = hex(i);
+                for(int j = 0; j < 2-curr.size(); j++) ans+="0";
+                ans+=curr;
+            }
+            return ans;
+        }
+        else if(Mem_Write==1) {
+            for(int i = 0; i < numBytes; ++i) {
+                std::string temp = "0xFF";
+                for(int j = 0; j < 2*i; j++) temp+="0";
+                int x = std::strtol(temp.c_str(), nullptr, 16);
+                dataMemory[MAR][i] = (MDR & x)>>(8*i);
+            }
+            return "0x1";
         }
     }
+    else {
+        std::vector<int> t = instructionMemory[MAR];
+        std::string ans = "";
+        int x = t.size();
+        for(int i = 0; i < x; ++i) ans+=(t[x-1-i]+'0');
+        ans = "0x"+ans;
+        return ans;
+    }
+}
+
+// Utility function to convert long integer to hexadecimal string
+std::string hex(long n) {
+    std::string ans = "";
+    while (n != 0) {
+        int rem = 0;
+        char ch;
+        rem = n % 16;
+        if (rem < 10) {
+            ch = rem + 48;
+        }
+        else {
+            ch = rem + 55;
+        }
+        ans += ch;
+        n = n / 16;
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
 }

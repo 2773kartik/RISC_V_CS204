28014d4edc9d0c9df510a62bbc83ae8c32334fb2
Harsh Raj Srivastava <harshrajsrivastava03@gmail.com>
2023-04-07 02:24:09 +0530

    Added main.py

---
+++ phase2/src/main.py
@@ -0,0 +1,19 @@
+from processor import Processor
+from state import State
+from hdu import HDU
+from btb import BTB
+
+stats = [
+	"Total number of cycles: ",
+	"Total instructions executed: ",
+	"CPI: ",
+	"Number of data-transfer(load and store): ",
+	"Number of ALU instructions executed: ",
+	"Number of Control instructions: ",
+	"Number of stalls/bubbles in the pipeline: ",
+	"Number of data hazards: ",
+	"Number of control hazards: ",
+	"Number of branch mispredictions: ",
+	"Number of stalls due to data hazards: ",
+	"Number of stalls due to control hazards: "
+]

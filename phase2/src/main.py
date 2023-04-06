0a70bc4b9673b6cdcd135ae28503bc546032842a
Harshit Gupta <2021csb1092@iitrpr.ac.in>
2023-04-07 02:31:41 +0530

    btb

--- phase2/src/main.py
+++ phase2/src/main.py
@@ -17,3 +17,20 @@ stats = [
 	"Number of stalls due to data hazards: ",
 	"Number of stalls due to control hazards: "
 ]
+
+control_hazard_signals = []
+
+def evaluate(processor, pipeline_ins):
+	processor.write_back(pipeline_ins[0])
+	processor.mem(pipeline_ins[1])
+	processor.execute(pipeline_ins[2])
+	control_hazard, control_pc, entering, color = processor.decode(pipeline_ins[3], btb)
+	if entering:
+		control_hazard_signals.append(2)
+	elif pipeline_ins[2].is_dummy and color != 0 and len(control_hazard_signals) > 0 and control_hazard_signals[-1] == 2:
+		control_hazard_signals.append(control_hazard_signals[-1])
+	else:
+		control_hazard_signals.append(color)
+	processor.fetch(pipeline_ins[4], btb)
+	pipeline_ins = [pipeline_ins[1], pipeline_ins[2], pipeline_ins[3], pipeline_ins[4]]
+	return pipeline_ins, control_hazard, control_pc
